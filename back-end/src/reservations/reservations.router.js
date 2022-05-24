056ff65a7046ae447dc7e1510381ff810bc07675
Mark <kdvsmm@gmail.com>
2022-05-24 10:49:03 -0400

    First update

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,9 +4,25 @@
  * @type {Router}
  */
 
-const router = require("express").Router();
-const controller = require("./reservations.controller");
-
-router.route("/").get(controller.list);
-
-module.exports = router;
+ const router = require("express").Router();
+ const methodNotAllowed = require("../errors/methodNotAllowed");
+ const controller = require("./reservations.controller");
+ 
+ router
+   .route("/:reservation_Id/status")
+   .put(controller.update)
+   .all(methodNotAllowed);
+ 
+ router
+   .route("/:reservation_Id")
+   .get(controller.read)
+   .put(controller.modify)
+   .all(methodNotAllowed);
+ 
+ router
+   .route("/")
+   .get(controller.list)
+   .post(controller.create)
+   .all(methodNotAllowed);
+ 
+ module.exports = router;
